a63222cbe7cad9d6c3046d9fa60264de3e3b4738
Loci_Natit <l_natit@outlook.com>
2024-09-16 16:43:19 +0800

    Fix client inconsistency Bump version to 1.0.1

--- src/main/java/com/lnatit/retrorain/mixin/client/RetroRainMixinLevel.java
+++ src/main/java/com/lnatit/retrorain/mixin/client/RetroRainMixinLevel.java
@@ -2,7 +2,7 @@ package com.lnatit.retrorain.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.lnatit.retrorain.RetroRain;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +12,8 @@ public class RetroRainMixinLevel
 {
     @ModifyReturnValue(method = "getRainLevel", at = @At("RETURN"))
     private float retrorainInject$getRainLevel(float original) {
+        if (!((Level)(Object) this instanceof ClientLevel))
+            return original;
         original = Math.max(original, RetroRain.RetroRainClient.rainManager.getRainLevel());
 //        if (Minecraft.getInstance().cameraEntity != null) {
 //            return Minecraft.getInstance().cameraEntity.getY() <= 196 ? original : 0.0f;
